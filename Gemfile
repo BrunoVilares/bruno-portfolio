5c85cb8e4a9e36e310432e071bf16af31cc7877a
BrunoVilares <brunovilares@live.ie>
2017-10-09 15:59:14 +0100

    update portfolio page

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem "font-awesome-rails"
+
+
 
 #Heroku
 gem 'pg', '0.18.1', group: :production
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
 	root 'pages#welcome'
+
+	get 'welcome' => 'pages#welcome'
+	get 'portfolio' => 'portfolio#portfolio'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
