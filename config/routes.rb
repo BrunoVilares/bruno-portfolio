d8c25f53209e74285ef446cae5484cf916df8c50
BrunoVilares <brunovilares@live.ie>
2017-10-19 11:07:08 +0100

    Create about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
 	get 'welcome' => 'pages#welcome'
 	get 'portfolio' => 'portfolio#portfolio'
+	get 'about' => 'about#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
