054f1303428965adf36c8b07d1df6c025ff2cf3d
BrunoVilares <brunovilares@live.ie>
2017-10-19 11:45:12 +0100

    Create Contact Page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 	get 'welcome' => 'pages#welcome'
 	get 'portfolio' => 'portfolio#portfolio'
 	get 'about' => 'about#about'
+	get 'contact' => 'contact#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
